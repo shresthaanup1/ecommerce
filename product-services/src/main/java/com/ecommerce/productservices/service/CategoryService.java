49932eeff55cbff3f9417e49cc1f81617207d67c
Nirmala100 <Nirmala100@users.noreply.github.com>
2023-02-05 10:43:07 -0800

    feature:refactoring service layer to implement interface

--- product-services/src/main/java/com/ecommerce/productservices/service/CategoryService.java
+++ product-services/src/main/java/com/ecommerce/productservices/service/CategoryService.java
@@ -1,46 +1,12 @@
 package com.ecommerce.productservices.service;
 
-import com.ecommerce.productservices.dao.CategoryDAO;
-import com.ecommerce.productservices.dto.CategoryDTO;
 import com.ecommerce.productservices.model.AddCategoryRequest;
 import com.ecommerce.productservices.model.Category;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
-@Service
-public class CategoryService {
-    @Autowired
-    private CategoryDAO categoryDAO;
-
-    public Category addCategory(AddCategoryRequest addCategoryRequest) {
-        CategoryDTO categoryDTO = new CategoryDTO(addCategoryRequest.getCategoryName(), addCategoryRequest.getCategoryDescription());
-        categoryDTO = categoryDAO.save(categoryDTO);
-        return new Category(categoryDTO.getId(), categoryDTO.getCategoryName(), categoryDTO.getCategoryDescription());
-    }
-
-
-    public List<Category> listCategories() {
-        List<CategoryDTO> categoryDTOs = categoryDAO.findAll();
-        List<Category> categories = new ArrayList<>();
-        for (CategoryDTO categoryDTO : categoryDTOs) {
-            categories.add(new Category(categoryDTO.getId(),categoryDTO.getCategoryName(),categoryDTO.getCategoryDescription()));
-        }
-        return categories;
-    }
-
-    public Category getCategoryById(Long id) {
-        Optional<CategoryDTO> optionalCategoryDTO = categoryDAO.findById(id);
-        if (optionalCategoryDTO.isPresent()) {
-            CategoryDTO categoryDTO = optionalCategoryDTO.get();
-            Category category = new Category(categoryDTO.getId(),categoryDTO.getCategoryName(),categoryDTO.getCategoryDescription());
-            return category;
-        } else {
-            return new Category();
-        }
-    }
+public interface CategoryService {
+     Category addCategory(AddCategoryRequest addCategoryRequest);
+     List<Category> listCategories();
+     Category getCategoryById(Long id);
 }
---
+++ product-services/src/main/java/com/ecommerce/productservices/service/CategoryServiceImpl.java
@@ -0,0 +1,45 @@
+package com.ecommerce.productservices.service;
+
+import com.ecommerce.productservices.dao.CategoryDAO;
+import com.ecommerce.productservices.dto.CategoryDTO;
+import com.ecommerce.productservices.model.AddCategoryRequest;
+import com.ecommerce.productservices.model.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    @Autowired
+    private CategoryDAO categoryDAO;
+    @Override
+    public Category addCategory(AddCategoryRequest addCategoryRequest) {
+        CategoryDTO categoryDTO = new CategoryDTO(addCategoryRequest.getCategoryName(), addCategoryRequest.getCategoryDescription());
+        categoryDTO = categoryDAO.save(categoryDTO);
+        return new Category(categoryDTO.getId(), categoryDTO.getCategoryName(), categoryDTO.getCategoryDescription());
+    }
+
+    @Override
+    public List<Category> listCategories() {
+        List<CategoryDTO> categoryDTOs = categoryDAO.findAll();
+        List<Category> categories = new ArrayList<>();
+        for (CategoryDTO categoryDTO : categoryDTOs) {
+            categories.add(new Category(categoryDTO.getId(),categoryDTO.getCategoryName(),categoryDTO.getCategoryDescription()));
+        }
+        return categories;
+    }
+    @Override
+    public Category getCategoryById(Long id) {
+        Optional<CategoryDTO> optionalCategoryDTO = categoryDAO.findById(id);
+        if (optionalCategoryDTO.isPresent()) {
+            CategoryDTO categoryDTO = optionalCategoryDTO.get();
+            Category category = new Category(categoryDTO.getId(),categoryDTO.getCategoryName(),categoryDTO.getCategoryDescription());
+            return category;
+        } else {
+            return new Category();
+        }
+    }
+}
