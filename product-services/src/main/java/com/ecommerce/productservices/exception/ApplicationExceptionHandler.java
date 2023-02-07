2861280352f37242dbb97b5c94c874f32b227d8e
Nirmala100 <shrestha.nirmala100@gmail.com>
2023-02-07 09:26:14 -0800

    feature:EC-24: Implement errorResponse class for sending errors

--- product-services/src/main/java/com/ecommerce/productservices/exception/ApplicationExceptionHandler.java
+++ product-services/src/main/java/com/ecommerce/productservices/exception/ApplicationExceptionHandler.java
@@ -6,16 +6,20 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Arrays;
+
 @ControllerAdvice
 public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler({CategoryNotFoundException.class})
     public ResponseEntity<Object> handleResourceNotFoundException(RuntimeException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        ErrorResponse errorResponse = new ErrorResponse(Arrays.asList(ex.getLocalizedMessage()));
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({JsonParameterNotValidException.class})
     public ResponseEntity<Object> JsonParameterNotValidException(RuntimeException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        ErrorResponse errorResponse = new ErrorResponse(Arrays.asList(ex.getLocalizedMessage()));
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
 
