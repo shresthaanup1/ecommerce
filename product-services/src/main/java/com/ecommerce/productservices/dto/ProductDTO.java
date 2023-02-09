8c2a553988211cae5d446192ad475cca09600710
Nirmala100 <shrestha.nirmala100@gmail.com>
2023-02-08 18:09:12 -0800

    feature:EC-26: created entity for product table

---
+++ product-services/src/main/java/com/ecommerce/productservices/dto/ProductDTO.java
@@ -0,0 +1,100 @@
+package com.ecommerce.productservices.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(schema="ecommerce",name = "product")
+public class ProductDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "product_name")
+    private String productName;
+    @Column(name = "product_price")
+    private Double productPrice;
+    @Column(name = "is_active")
+    private boolean isActive;
+    @Column(name = "is_available")
+    private boolean isAvailable;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    private CategoryDTO categoryDTO;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(String productName, Double productPrice, boolean isActive, boolean isAvailable, LocalDateTime createdAt, CategoryDTO categoryDTO) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.isActive = isActive;
+        this.isAvailable = isAvailable;
+        this.createdAt = createdAt;
+        this.categoryDTO = categoryDTO;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(Double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public CategoryDTO getCategoryDTO() {
+        return categoryDTO;
+    }
+
+    public void setCategoryDTO(CategoryDTO categoryDTO) {
+        this.categoryDTO = categoryDTO;
+    }
+}
