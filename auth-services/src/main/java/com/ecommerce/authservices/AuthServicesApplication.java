6cfead254fb4bef91370d40a476a224e698fd608
Nirmala100 <shrestha.nirmala100@gmail.com>
2023-02-15 11:25:08 -0800

    feature:EC-4:Created skeleton for auth service

---
+++ auth-services/src/main/java/com/ecommerce/authservices/AuthServicesApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.authservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServicesApplication.class, args);
+	}
+
+}
