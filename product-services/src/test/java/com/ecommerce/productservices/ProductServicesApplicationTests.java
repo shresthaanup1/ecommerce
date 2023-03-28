32ac7fa4d64ca915a3aacf07aafbbc42f5cbc901
ShresthaAnup <adshrestha1@gmail.com>
2023-03-28 10:38:24 -0700

    feature: added one test method for integration testing in product-services microservice.

--- product-services/src/test/java/com/ecommerce/productservices/ProductServicesApplicationTests.java
+++ product-services/src/test/java/com/ecommerce/productservices/ProductServicesApplicationTests.java
@@ -1,13 +1,43 @@
 package com.ecommerce.productservices;
 
+import com.ecommerce.productservices.controller.CategoryController;
+import com.netflix.discovery.converters.Auto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ProductServicesApplicationTests {
+
+	@Autowired
+	private CategoryController categoryController;
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(categoryController);
+		assertNotNull(mockMvc);
 	}
 
+	@Test
+	public void getCategoryByIdTest() throws Exception{
+		RequestBuilder request = MockMvcRequestBuilders.get("/product/category/1");
+		mockMvc.perform(request)
+				.andExpect(status().isFound())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+		//.andExpectAll()
+	}
 }
